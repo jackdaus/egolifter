04fda0ebd0af3fedd31908ec67c722d073f022d3
Jack Daus <jackdaus@gmail.com>
2025-04-19 21:50:11 -0400

    Tweak preprocessing scripts

--- scripts/download_process_adt.bash
+++ scripts/download_process_adt.bash
@@ -17,92 +17,94 @@ cp assets/vignette_ov7251.png ${ADT_DATA_ROOT} # Vignette image for the SLAM cam
 # https://github.com/facebookresearch/projectaria_tools/blob/main/projects/AriaDigitalTwinDatasetTools/data_provider/AriaDigitalTwinDataPathsProvider.h#L38
 
 # scene names using the old data format
-declare -a SCENE_NAMES=(
-    "Apartment_release_multiskeleton_party_seq121"
-    "Apartment_release_multiskeleton_party_seq122"
-    "Apartment_release_multiskeleton_party_seq123"
-    "Apartment_release_multiskeleton_party_seq125"
-    "Apartment_release_multiskeleton_party_seq126"
-    "Apartment_release_multiskeleton_party_seq127"
-    "Apartment_release_decoration_skeleton_seq137"
-    "Apartment_release_meal_skeleton_seq136"
-    "Apartment_release_multiuser_clean_seq116"
-    "Apartment_release_multiuser_cook_seq114"
-    "Apartment_release_multiuser_cook_seq143"
-    "Apartment_release_multiuser_meal_seq132"
-    "Apartment_release_multiuser_meal_seq140"
-    "Apartment_release_multiuser_party_seq140"
-    "Apartment_release_work_skeleton_seq131"
-    "Apartment_release_work_skeleton_seq140"
-)
+# JD TEMP: test with one scene
+# declare -a SCENE_NAMES=(
+#     "Apartment_release_multiskeleton_party_seq121"
+#     "Apartment_release_multiskeleton_party_seq122"
+#     "Apartment_release_multiskeleton_party_seq123"
+#     "Apartment_release_multiskeleton_party_seq125"
+#     "Apartment_release_multiskeleton_party_seq126"
+#     "Apartment_release_multiskeleton_party_seq127"
+#     "Apartment_release_decoration_skeleton_seq137"
+#     "Apartment_release_meal_skeleton_seq136"
+#     "Apartment_release_multiuser_clean_seq116"
+#     "Apartment_release_multiuser_cook_seq114"
+#     "Apartment_release_multiuser_cook_seq143"
+#     "Apartment_release_multiuser_meal_seq132"
+#     "Apartment_release_multiuser_meal_seq140"
+#     "Apartment_release_multiuser_party_seq140"
+#     "Apartment_release_work_skeleton_seq131"
+#     "Apartment_release_work_skeleton_seq140"
+# )
 
 # scene names using the new format, only used for downloading script
-declare -a SCENE_NAMES_NEW=(
-    "Apartment_release_multiskeleton_party_seq121_71292"
-    "Apartment_release_multiskeleton_party_seq121_M1292"
-    "Apartment_release_multiskeleton_party_seq122_71292"
-    "Apartment_release_multiskeleton_party_seq122_M1292"
-    "Apartment_release_multiskeleton_party_seq123_71292"
-    "Apartment_release_multiskeleton_party_seq123_M1292"
-    "Apartment_release_multiskeleton_party_seq125_71292"
-    "Apartment_release_multiskeleton_party_seq125_M1292"
-    "Apartment_release_multiskeleton_party_seq126_71292"
-    "Apartment_release_multiskeleton_party_seq126_M1292"
-    "Apartment_release_multiskeleton_party_seq127_71292"
-    "Apartment_release_multiskeleton_party_seq127_M1292"
-    "Apartment_release_decoration_skeleton_seq137_M1292"
-    "Apartment_release_meal_skeleton_seq136_M1292"
-    "Apartment_release_multiuser_clean_seq116_M1292"
-    "Apartment_release_multiuser_cook_seq114_M1292"
-    "Apartment_release_multiuser_cook_seq143_M1292"
-    "Apartment_release_multiuser_meal_seq132_M1292"
-    "Apartment_release_multiuser_meal_seq140_M1292"
-    "Apartment_release_multiuser_party_seq140_M1292"
-    "Apartment_release_work_skeleton_seq131_M1292"
-    "Apartment_release_work_skeleton_seq140_M1292"
-)
-
-# # Only for debugging
 # declare -a SCENE_NAMES_NEW=(
 #     "Apartment_release_multiskeleton_party_seq121_71292"
 #     "Apartment_release_multiskeleton_party_seq121_M1292"
-# )
-# declare -a SCENE_NAMES=(
-#     "Apartment_release_multiskeleton_party_seq121"
+#     "Apartment_release_multiskeleton_party_seq122_71292"
+#     "Apartment_release_multiskeleton_party_seq122_M1292"
+#     "Apartment_release_multiskeleton_party_seq123_71292"
+#     "Apartment_release_multiskeleton_party_seq123_M1292"
+#     "Apartment_release_multiskeleton_party_seq125_71292"
+#     "Apartment_release_multiskeleton_party_seq125_M1292"
+#     "Apartment_release_multiskeleton_party_seq126_71292"
+#     "Apartment_release_multiskeleton_party_seq126_M1292"
+#     "Apartment_release_multiskeleton_party_seq127_71292"
+#     "Apartment_release_multiskeleton_party_seq127_M1292"
+#     "Apartment_release_decoration_skeleton_seq137_M1292"
+#     "Apartment_release_meal_skeleton_seq136_M1292"
+#     "Apartment_release_multiuser_clean_seq116_M1292"
+#     "Apartment_release_multiuser_cook_seq114_M1292"
+#     "Apartment_release_multiuser_cook_seq143_M1292"
+#     "Apartment_release_multiuser_meal_seq132_M1292"
+#     "Apartment_release_multiuser_meal_seq140_M1292"
+#     "Apartment_release_multiuser_party_seq140_M1292"
+#     "Apartment_release_work_skeleton_seq131_M1292"
+#     "Apartment_release_work_skeleton_seq140_M1292"
 # )
 
-# Download
-for SCENE_NAME in "${SCENE_NAMES_NEW[@]}"; do
-    aria_dataset_downloader \
-        -c ${ADT_DATA_ROOT}/ADT_download_urls.json \
-        -o ${ADT_DATA_ROOT}/ \
-        -d 0 1 2 3 6 7 \
-        -l ${SCENE_NAME}
-done
+# # Only for debugging
+declare -a SCENE_NAMES_NEW=(
+    "Apartment_release_golden_skeleton_seq100_10s_sample_M1292"
+)
+declare -a SCENE_NAMES=(
+    "Apartment_release_golden_skeleton_seq100_10s_sample"
+)
+
+# # Download
+# for SCENE_NAME in "${SCENE_NAMES_NEW[@]}"; do
+#     aria_dataset_downloader \
+#         -c ${ADT_DATA_ROOT}/ADT_download_urls.json \
+#         -o ${ADT_DATA_ROOT}/ \
+#         -d 0 1 2 3 6 7 \
+#         -l ${SCENE_NAME}
+# done
 
 # Rectify images and format the folder structure
-for SCENE_NAME in "${SCENE_NAMES[@]}"; do
-    python scripts/process_adt_3dgs.py \
-        --data_root ${ADT_DATA_ROOT} \
-        --output_root ${ADT_PROCESSED_ROOT} \
-        --sequence_name ${SCENE_NAME} &
-done
+# for SCENE_NAME in "${SCENE_NAMES[@]}"; do
+#     python scripts/process_adt_3dgs.py \
+#         --data_root ${ADT_DATA_ROOT} \
+#         --output_root ${ADT_PROCESSED_ROOT} \
+#         --sequence_name ${SCENE_NAME}
+# done
 
 wait
 
 # Generate SAM segmentation results
+# NOTE JD: Using aggressive stride to speed up the process for testing
 for SCENE_NAME in "${SCENE_NAMES[@]}"; do
     python scripts/generate_gsa_results.py \
         -i ${ADT_PROCESSED_ROOT}/${SCENE_NAME} \
         --class_set none \
         --sam_variant sam \
         --max_longer_side 512 \
-        --no_clip
+        --no_clip \
+        --stride 10
 done
 
 # Generate evaluation target for query-based segmentation
 for SCENE_NAME in "${SCENE_NAMES[@]}"; do
-    python scripts/generate_2dseg_query.py \
+    uv run scripts/generate_2dseg_query.py \
         --data_root ${ADT_PROCESSED_ROOT} \
         --scene_name ${SCENE_NAME} &
 done
@@ -110,13 +112,13 @@ done
 wait
 
 for SCENE_NAME in "${SCENE_NAMES[@]}"; do
-    python scripts/generate_2dseg_query_sample.py \
+    uv run scripts/generate_2dseg_query_sample.py \
         --data_root ${ADT_PROCESSED_ROOT} \
         --scene_name ${SCENE_NAME}
 done
 
 for SCENE_NAME in "${SCENE_NAMES[@]}"; do
-    python scripts/generate_3dbox_query.py \
+    ur run scripts/generate_3dbox_query.py \
         --raw_root ${ADT_DATA_ROOT} \
         --data_root ${ADT_PROCESSED_ROOT} \
         --scene_name ${SCENE_NAME}
--- uv-readme.md
+++ uv-readme.md
@@ -13,3 +13,88 @@ uv sync
 ```bash
 uvx wandb login
 ```
+
+# Download Models
+
+```bash
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+wget https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
+```
+
+# Set up Grounded-Segment-Anything
+
+```bash
+git clone https://github.com/IDEA-Research/Grounded-Segment-Anything.git
+
+cd Grounded-Segment-Anything/
+
+export AM_I_DOCKER=False
+export BUILD_WITH_CUDA=True
+
+uv pip install -e ./segment_anything
+
+uv pip install -e ./GroundingDINO
+
+uv pip install --upgrade "diffusers[torch]"
+```
+
+# To run the pipeline on the ADT data
+
+Set up environment variables.
+
+```bash
+source setup_env.bash
+```
+
+## Download Data
+
+First access ADT through this link and download the ADT_download_urls.json file, which contains the download links for the dataset.
+
+Then prepare a directory where you want to save the downloaded and processed dataset as follows. And put the ADT_download_urls.json in the $ADT_DATA_ROOT directory.
+
+```bash
+# Change the following to directories where you want to save the dataset
+export ADT_DATA_ROOT=adt
+export ADT_PROCESSED_ROOT=adt_processed
+
+mkdir -p $ADT_DATA_ROOT
+mkdir -p $ADT_PROCESSED_ROOT
+
+cp /path/to/ADT_download_urls.json $ADT_DATA_ROOT
+```
+
+Then run the following script to download and process the dataset.
+
+```bash
+uv run bash scripts/download_process_adt.bash
+```
+
+# Run visualizer of an ADT dataset (TODO make work with WSL)
+
+For WSL, you can run the rerun server to enable viewing on windows. This will forward the app to localhost:9090. (To run this command, make sure the python venv is activated.)
+
+```bash
+rerun --serve-web
+```
+
+And then create a .rrd file from a sequence. This file can then be loaded into rerun.
+
+```bash
+uvx --from projectaria-tools viewer_projects_adt --sequence_path ./path/to/adt_sequence --rrd_output_path adt_dataset.rrd
+```
+
+for example, 
+
+```bash
+uvx --from projectaria-tools viewer_projects_adt --sequence_path ./adt/Apartment_release_multiskeleton_party_seq121_71292 --rrd_output_path Apartment_release_multiskeleton_party_seq121_71292.rrd
+```
+
+TODO: figure out how to stream data to rerun... not working for me in wsl.
+
+# Jupyter
+
+To create a Jupyter kernel, run, 
+
+```bash
+uv run ipython kernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=egolifter
+```
